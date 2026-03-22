31bf066938e8dd7685972554990d3c4fd15ed3e5
Alexis Leclerc <web@alxs.fr>
2026-03-22 21:54:35 +0100

    Renaming test

--- e2e/image-tag-detail.test.ts
+++ e2e/image-tag-detail.test.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { login } from './helpers/login';
 
-test.describe('Tag detail page', () => {
+test.describe('Image tag detail page', () => {
 	test.beforeEach(async ({ page }) => {
 		await login(page);
 	});
